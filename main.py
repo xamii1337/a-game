63a6c2b93bd4956a0c8f82adfb75087b01755104
xamii1337 <mikityukgleb@gmail.com>
2025-04-19 14:33:42 +0500

    added working walking sound

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 from pygame.mixer import find_channel
 
+# Настройки окна
 WIDTH = 1920
 HEIGHT = 1080
 FPS = 30
 
-# создаем игру и окно
+# Инициализация игры и аудио
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -18,13 +19,17 @@ img_surf = pygame.transform.scale(img_surf, (WIDTH, HEIGHT))
 img_hero = pygame.image.load("./hero.png").convert_alpha()
 img_hero2 = pygame.image.load("./hero2.png").convert_alpha()
 
-walk = ('walk_sound.mp3')
+# Звук шагов
+walk_sound = pygame.mixer.Sound('walk_sound.mp3')
+walk_channel = pygame.mixer.Channel(1)  # Отдельный канал для шагов
 
+# Фоновая музыка
 music_files = ["music1.mp3", "music2.mp3", "music3.mp3"]
 current_track = 0
 pygame.mixer.music.load(music_files[current_track])
 pygame.mixer.music.play()
 
+# Класс персонажа
 class Hero(pygame.sprite.Sprite):
     def __init__(self, name, age, image, x, y):
         super().__init__()
@@ -48,17 +53,19 @@ class Hero(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.rect.top = HEIGHT
 
+# Создание героев
 hero1 = Hero("hero", 40, img_hero, 500, 750)
 hero2 = Hero("hero2", 16, img_hero2, 750, 750)
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(hero1, hero2)
 
+# Главный игровой цикл
 running = True
 while running:
-    # Проверка, закончилась ли музыка
-    if not pygame.mixer.music.get_busy():  # если не играет
-        current_track = (current_track + 1) % len(music_files)  # следующий трек
+    # Переключение фоновой музыки при окончании трека
+    if not pygame.mixer.music.get_busy():
+        current_track = (current_track + 1) % len(music_files)
         pygame.mixer.music.load(music_files[current_track])
         pygame.mixer.music.play()
 
@@ -66,7 +73,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # Движение героев
         elif event.type == pygame.KEYDOWN:
+            # Hero1 (стрелки)
             if event.key == pygame.K_LEFT:
                 hero1.x_change = -5
             if event.key == pygame.K_RIGHT:
@@ -76,6 +85,7 @@ while running:
             if event.key == pygame.K_DOWN:
                 hero1.y_change = 5
 
+            # Hero2 (WASD)
             if event.key == pygame.K_a:
                 hero2.x_change = -10
             if event.key == pygame.K_d:
@@ -85,17 +95,28 @@ while running:
             if event.key == pygame.K_s:
                 hero2.y_change = 10
 
+            # Воспроизведение звука шагов
+            if not walk_channel.get_busy():
+                walk_channel.play(walk_sound, loops=-1)
+
         elif event.type == pygame.KEYUP:
+            # Hero1
             if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
                 hero1.x_change = 0
             if event.key in (pygame.K_UP, pygame.K_DOWN):
                 hero1.y_change = 0
 
+            # Hero2
             if event.key in (pygame.K_a, pygame.K_d):
                 hero2.x_change = 0
             if event.key in (pygame.K_w, pygame.K_s):
                 hero2.y_change = 0
 
+            # Если оба героя не двигаются, остановить звук шагов
+            if hero1.x_change == 0 and hero1.y_change == 0 and hero2.x_change == 0 and hero2.y_change == 0:
+                walk_channel.stop()
+
+    # Отрисовка
     screen.blit(img_surf, (0, 0))
     all_sprites.update()
     all_sprites.draw(screen)
